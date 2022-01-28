fc958d253a232a87c536a7a1174df33b3737708f
Kitton._ <tonkittipod@gmail.com>
2022-01-28 23:05:37 +0700

    Learn C

---
+++ C/02-Variables/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int age,age2;
+    char name[] = "Kittipod";
+    char m = "m";
+    age = 17; age2 = 18;
+
+    printf("My name is %s.\n",name);
+    printf("I'%c %d %d years old.",m,age,age2);
+    return 0;
+}
