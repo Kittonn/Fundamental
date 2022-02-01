cf7dba9a06c4df3e6b71820fb9be036d1e06e66d
Kitton._ <tonkittipod@gmail.com>
2022-02-01 15:43:07 +0700

    Learn C++

---
+++ C++/02-Variables/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name = "Kittipod";
+    int age = 17;
+    cout << "My name is " << name << " Lambangchang" << endl;
+    cout << "I am " << age << " years old.";
+    return 0;
+}
---
+++ C++/03-Data Types/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    char grade = 'A'; // character
+    string text = "Hello World"; // string
+    int num = 1; // integer
+    double de = 15.2636465; // float
+    bool tf = true; // boolean
+    
+
+    return 0;
+}
