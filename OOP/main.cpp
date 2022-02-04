5c0c54151363f9e1b62c9e0c36c58330d34daf78
Kitton._ <tonkittipod@gmail.com>
2022-02-04 15:16:19 +0700

    Learn OOP

--- OOP/main.cpp
+++ OOP/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+using namespace std;
+
+class Employee {
+    
+    public:
+        // attributes
+        string Name;
+        string Company;
+        int Age;
+
+        void sayHello(){
+            cout << "Hello World";
+        }
+
+        Employee(string aName, string aCompany,int aAge) {
+            Age = aAge;
+            Name = aName;
+            Company = aCompany;
+        } 
+        
+};
 
 int main(){
+    
+    
+    Employee employee1("Kittipod","LA",18);
+
+    employee1.sayHello();
+    
+    cout << endl << employee1.Name;
     
     return 0;
 }
