e8a6336b0cf916ed0ff89efbab5fbb8de5275423
Kitton._ <tonkittipod@gmail.com>
2022-02-05 16:55:40 +0700

    OOP Exercise

---
+++ OOP/Exercises/7.cpp
@@ -0,0 +1,6 @@
+/*
+    Write a probram with a mother class animal. Inside it define a name and 
+    an age variables, and set_value() function.Then create two bases variables 
+    Zebra and Dolphin which write a message telling the age, the name and giving 
+    some extra information (e.g. place of origin).
+*/
