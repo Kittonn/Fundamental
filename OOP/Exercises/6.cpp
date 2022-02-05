7900e5785302b2cdc695f2ed9a06c52444552d4e
Kitton._ <tonkittipod@gmail.com>
2022-02-05 23:09:55 +0700

    OOP Exercise

--- OOP/Exercises/6.cpp
+++ OOP/Exercises/6.cpp
@@ -4,3 +4,32 @@
     (different for mother and daugther).In the main define a daughter 
     and call the display() method on it.
 */
+
+#include <iostream>
+using namespace std;
+
+class Mother {
+    public:
+        virtual void display(){
+            cout << "Mother Class." << endl;
+        }
+};
+
+class Daughter : public Mother {
+    public:
+        void display(){
+            cout << "Daughter Class.";
+        }
+};
+
+int main(){
+    Daughter d;
+    Mother m;
+    Mother *m1 = &m;
+    Mother *m2 = &d;
+
+    m1 ->display();
+    m2 ->display();
+
+    return 0;
+}
--- OOP/Exercises/7.cpp
+++ OOP/Exercises/7.cpp
@@ -4,3 +4,52 @@
     Zebra and Dolphin which write a message telling the age, the name and giving 
     some extra information (e.g. place of origin).
 */
+
+#include <iostream>
+using namespace std;
+
+class Animal {
+    protected:
+        string Name;
+        int Age;
+    public:
+        Animal(string name,int age){
+            Name = name;
+            Age = age;
+        }
+};
+
+class Zebra:public Animal {
+    private:
+        string Information;
+    public:
+        Zebra(string name, int age, string information):Animal(name,age){
+            Information = information;
+        }
+        void message(){
+            cout << Name << " " << Age << " " << Information << endl;
+        }
+};
+
+class Dolphin:public Animal {
+    private:
+        string Information;
+    public:
+        Dolphin(string name, int age, string information):Animal(name,age){
+            Information = information;
+        }
+        void message(){
+            cout << Name << " " << Age << " " << Information << endl;
+        }
+};
+
+
+int main(){
+    Dolphin d("Dora", 18,"From Greenland");
+    Zebra z("Zero",23,"From Africa");
+
+    d.message();
+    z.message();
+
+    return 0;
+}
