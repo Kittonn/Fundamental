1010046cbafbff634a718edd202131d53f5e37b7
Kitton._ <tonkittipod@gmail.com>
2022-02-05 20:56:54 +0700

    OOP Exercise

--- OOP/Exercises/4.cpp
+++ OOP/Exercises/4.cpp
@@ -3,3 +3,42 @@
     The program should ask for real and imaginary part of two complex numbers, 
     and display the real and imaginary parts of their sum.
 */
+
+#include <iostream>
+using namespace std;
+
+class Number {
+    private:
+        double X,Y;
+    public:
+        Number(double x,double y){
+            X = x;
+            Y = y;
+        }
+
+        double getX(){
+            return X;
+        }
+
+        double getY(){
+            return Y;
+        }
+
+
+    
+
+};
+
+int main(){
+
+    double a,b,c,d;
+    cout << "Enter real and imaginary part of Number : ";
+    cin >> a >> b;
+    cout << "Enter real and imaginary part of Number : ";
+    cin >> c >> d;
+    Number n1(a,b);
+    Number n2(c,d);
+    cout << "Sum : " << n1.getX() + n2.getX() << " " << n1.getY() + n2.getY();
+
+    return 0;
+}
--- OOP/Exercises/5.cpp
+++ OOP/Exercises/5.cpp
@@ -5,3 +5,44 @@
     define two variables a triangle and a rectangle and then call the area() function 
     in this two varibles.
 */
+
+#include <iostream>
+using namespace std;
+
+class Shape {
+    protected:
+        double width,height;
+    public:
+        Shape(int w,int h){
+            width = w;
+            height = h;
+        }
+};
+
+class Tri: public Shape{
+    public:
+        Tri(double w,double h):Shape(w,h){};
+
+        double getAreaTri(){
+            return 0.5 * width * height;
+        }
+};
+
+class Rec:public Shape {
+    public:
+        Rec(double w,double h):Shape(w,h){};
+        double getAreaRec(){
+            return width * height;
+        }
+};
+
+int main(){
+    double w,h;
+    cin >> w >> h;
+    Tri t(w,h);
+    Rec r(w,h);
+    
+    cout << "Area of Triangle : " << t.getAreaTri() << endl << "Area of Rectangle : " << r.getAreaRec();
+    
+    return 0;
+}
