dd34ba7b36c567791ddb87f493bf2ba1ec907583
Kitton._ <tonkittipod@gmail.com>
2022-02-27 10:18:32 +0700

    Learn JavaScript

--- Website/JavaScript/script.js
+++ Website/JavaScript/script.js
@@ -1,3 +1,21 @@
-var x = 10;
-console.log(x);
-console.log("Hello World");
+const data = [
+    {
+        img: "img1",
+        title: "title1",
+        author: "author1"
+    },
+    {
+        img: "img2",
+        author: "author2",
+        title: "title2"
+    },
+    {
+        img: "img3",
+        author: "author3",
+        title: "title3"
+    }
+]
+console.log(data.map((book) => {
+    console.log({...book});
+}));
+
