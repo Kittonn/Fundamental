7a195e151605c52b4fdc6a6c2006f9c4d8464a60
Kitton._ <tonkittipod@gmail.com>
2022-04-19 13:26:05 +0700

    Learn Svelte

---
+++ Website/svelte/my-svelte-project/src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  let date = 8;
+  export let name;
+
+  const handleClick = () => {
+    date = 19;
+  };
+</script>
+
+<div>
+  <div>Hello {date} {name}</div>
+  <button on:click={handleClick}>Change</button>
+</div>
+
+<style>
+  div {
+    color: aquamarine;
+    background-color: black;
+  }
+</style>
