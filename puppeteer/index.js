1f95ea10d2e56b0e40bd96e006183a0a03938fce
Kitton._ <tonkittipod@gmail.com>
2022-05-06 01:41:07 +0700

    Web Scraping

--- puppeteer/index.js
+++ puppeteer/index.js
@@ -6,30 +6,37 @@ const start = async () => {
   const page = await browser.newPage();
   page.setDefaultNavigationTimeout(0);
   await page.goto(
-    "https://th.m.wikipedia.org/wiki/%E0%B8%A3%E0%B8%B2%E0%B8%A2%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%84%E0%B8%B3%E0%B8%82%E0%B8%A7%E0%B8%B1%E0%B8%8D%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B8%88%E0%B8%B3%E0%B8%88%E0%B8%B1%E0%B8%87%E0%B8%AB%E0%B8%A7%E0%B8%B1%E0%B8%94%E0%B8%82%E0%B8%AD%E0%B8%87%E0%B9%84%E0%B8%97%E0%B8%A2"
+    "https://th.wikipedia.org/wiki/%E0%B8%A3%E0%B8%B2%E0%B8%A2%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%84%E0%B8%B3%E0%B8%82%E0%B8%A7%E0%B8%B1%E0%B8%8D%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B8%88%E0%B8%B3%E0%B8%88%E0%B8%B1%E0%B8%87%E0%B8%AB%E0%B8%A7%E0%B8%B1%E0%B8%94%E0%B8%82%E0%B8%AD%E0%B8%87%E0%B9%84%E0%B8%97%E0%B8%A2"
   );
 
-  let name = [];
-  // #content-collapsible-block-0 > dl:nth-child(3) > dt > a
-  for (let i = 1; i < 19; i += 2) {
-    let element = await page.waitForSelector(
-      `#content-collapsible-block-0 > dl:nth-child(${i}) > dt > a`
-    );
-    let element2 = await page.waitForSelector(
-      `#content-collapsible-block-0 > ul:nth-child(${i + 1}) > li`
-    );
-    let text = await page.evaluate((element) => element.textContent, element);
-    let slogan = await page.evaluate(
-      (element) => element.textContent,
-      element2
-    );
+  // #mw-content-text > div.mw-parser-output > ul:nth-child(8) > li
+  const provinces = await page.evaluate(() => {
+    return Array.from(
+      document.querySelectorAll(".mw-parser-output dl dt a")
+    ).map((x) => x.textContent);
+  });
+  const slogans = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll(".mw-parser-output ul li"))
+      .map((x) => x.textContent)
+      .filter(
+        (x, i) =>
+          i >= 8 &&
+          i < 90 &&
+          i !== 20 &&
+          i !== 31 &&
+          i !== 74 &&
+          i !== 77 &&
+          i !== 79
+      );
+  });
+  console.log(slogans);
+  console.log(slogans.length);
 
-    await name.push(text, " ", slogan + "\n");
-  }
-  // #content-collapsible-block-0 > ul:nth-child(2) > li
-  // #content-collapsible-block-0 > ul:nth-child(18) > li
+  // let data = Object.assign(...provinces.map((k, i) => ({ [k]: slogans[i] })));
+  // Object.assign(...keys.map((k, i) => ({ [k]: values[i] })));
 
-  await fs.writeFile("name.txt", name);
+  // await fs.writeFile("slogans.json", JSON.stringify(data));
+  // await fs.writeFile("slogans.json", JSON.stringify(slogans));
 
   await browser.close();
 };
