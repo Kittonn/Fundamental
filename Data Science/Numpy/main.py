39db4c449170b5ac16020853af9189b3e5e2039a
Kitton._ <tonkittipod@gmail.com>
2022-05-12 11:18:51 +0700

    Numpy

---
+++ Data Science/Numpy/main.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+arr = np.array([[1, 23, 4], [3, 5, 6]], dtype='int16')
+arr2 = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+# print(arr2.ndim)
+# print(type(arr))
+# print(arr2.shape[0], arr2.shape[1], arr2.shape[2])
+# print(arr2[0, 1, 2])
+# a = np.arange(1, 101, 2).reshape(5, 10)
+# a = np.random.rand(100000)
+# plt.hist(a, bins=200)
+# plt.show()
+
+a = np.zeros((4, 2))
+# a = np.ones(10)
+# arr = np.arange(10)
+# print(arr)
+# a = np.random.permutation(arr)
+print(a)
