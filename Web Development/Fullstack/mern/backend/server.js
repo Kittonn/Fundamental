b7730c13ba6bd637d5df07bb31af54b8bc2027d7
Kittipod Lambangchang <tonkittipod@gmail.com>
2022-06-19 17:19:53 +0700

    🧨 MERN

--- Web Development/Fullstack/mern/backend/server.js
+++ Web Development/Fullstack/mern/backend/server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const colors = require("colors");
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.get("/api/goal", (req, res) => {
-  res.json({ message: "get goal" });
-});
-
-app.listen(port, () => console.log(`Server started on port:${port}`));
+connectDB();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+app.use(errorHandler);
+app.listen(port, () => console.log(`Server started on port:${port} 🚀`.red.underline));
